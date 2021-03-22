1babc2d7f6617c91b77ffcd8e7dca940b2dbc966
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2021-03-22 13:16:50 +0200

    :construction: Add AddWeekDays method

---
+++ 30 Seconds of CSharp/Date/AddWeekDays.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _30_Seconds_of_CSharp.Date
+{
+    public static partial class DateUtils
+    {
+        /// <summary>
+        /// Adds the given number of week days to the given DateTime.
+        /// </summary>
+        /// <param name="startingDate">The date to be changed.</param>
+        /// <param name="count">The number of weekdays to add</param>
+        /// <param name="holidays">An optional list of holidays to consider</param>
+        /// <returns>A DateTime increased by the given number of week days, considering holidays</returns>
+        public static DateTime AddWeekDays(DateTime startingDate, int count, IEnumerable<DateTime> holidays = null)
+        {
+            var days = Math.Abs(count);
+
+            for (int i = 0; i < days; i++)
+            {
+                do
+                {
+                    startingDate = startingDate.AddDays(1);
+                } while (
+                    startingDate.DayOfWeek == DayOfWeek.Saturday
+                 || startingDate.DayOfWeek == DayOfWeek.Sunday
+                 || holidays != null && holidays.Contains(startingDate.Date));
+            }
+
+            return startingDate;
+        }
+    }
+}
