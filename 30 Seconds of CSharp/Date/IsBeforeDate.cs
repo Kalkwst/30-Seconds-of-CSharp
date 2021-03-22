dcce3ab3f03df683328e4cfa3673f5d3e93ef05b
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2021-03-22 11:32:26 +0200

    :memo: Add documentation for function

--- 30 Seconds of CSharp/Date/IsBeforeDate.cs
+++ 30 Seconds of CSharp/Date/IsBeforeDate.cs
@@ -8,6 +8,12 @@ namespace _30_Seconds_of_CSharp.Date
 {
     public static partial class DateUtils
     {
+        /// <summary>
+        /// Checks if a date is before another date.
+        /// </summary>
+        /// <param name="dateA">The first date</param>
+        /// <param name="dateB">The second date</param>
+        /// <returns><c>true</c> if the first date is before the second date</returns>
         public static bool IsBeforeDate(DateTime dateA, DateTime dateB)
         {
             return dateA < dateB;
