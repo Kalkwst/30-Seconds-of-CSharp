9dc37a90ba034f99580ca0bd93ed528e97adc736
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2021-03-22 11:25:32 +0200

    :construction: Add IsBeforeDate function

---
+++ 30 Seconds of CSharp/Date/IsBeforeDate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_Seconds_of_CSharp.Date
+{
+    public static partial class DateUtils
+    {
+        public static bool IsBeforeDate(DateTime dateA, DateTime dateB)
+        {
+            return dateA < dateB;
+        }
+    }
+}
