56b224227e695092a301762969b32748bfd467aa
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2021-03-23 14:06:25 +0200

    :white_check_mark: Add tests for Tomorrow function

---
+++ 30 Seconds of CSharp Test/Date/Tomorrow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using _30_Seconds_of_CSharp.Date;
+using NUnit.Framework;
+
+namespace _30_Seconds_of_CSharp_Test.Date
+{
+    [TestFixture]
+    public class TomorrowTest
+    {
+        [Test]
+        public void TestTommorow()
+        {
+            DateTime t1 = DateTime.Now.AddDays(1);
+            DateTime t2 = DateTime.Parse(DateUtils.Tommorow());
+
+            Assert.AreEqual(t1.Year, t2.Year);
+            Assert.AreEqual(t1.Month, t2.Month);
+            Assert.AreEqual(t1.ToString("yyyy-MM-dd"), t2.ToString("yyyy-MM-dd"));
+        }
+    }
+}
