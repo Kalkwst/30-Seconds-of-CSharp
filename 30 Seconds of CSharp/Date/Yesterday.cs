2e608127ce4afbd2afd269791a75b4c1702fadd5
Kalkwst <kalafatiskwstas@gmail.com>
2021-03-23 14:57:14 +0200

    :sparkles: Add Date Utility methods (#4)
    * :construction: Add implementation and tests for FormatDuration function
    
    * :memo: Add FormatDuration snippet documentation
    
    * :memo: Fix formatting of snippet
    
    * :construction: Add IsBeforeDate function
    
    * :white_check_mark: Add tests for IsBeforeDate
    
    * :memo: Add documentation for function
    
    * :memo: Add IsBeforeDate snippet documentation
    
    * :memo: Change formatting of the snippet documentation
    
    * :construction: Add IsAfterDate method
    
    * :white_check_mark: Add tests for IsAfterDate method
    
    * :memo: Add documentation for IsAfterDate snippet
    
    * :construction: Add AddWeekDays method
    
    * :white_check_mark: Add tests for AddWeekDays method
    
    * :memo: Add documentation for AddWeekDays snippet
    
    * :construction: Add DaysAgo method
    
    * :white_check_mark: Add tests for DaysAgo method
    
    * :memo: Add documentation for DaysAgo snippet
    
    * :construction: Add DaysFromNow method
    
    * :white_check_mark: Add tests for DaysFromNow method
    
    * :memo: Add documentation for DaysFromNow snippet
    
    * :construction: Add GetColonTimeFromDate method
    
    * :construction: Add GetColonTimeFromDate method
    
    * :memo: Add documentation for the GetColonTimeFromDate snippet
    
    * :construction: Add GetDaysDiffBetweenDates method
    
    * :white_check_mark: Add tests for GetDaysDiffBetweenDatesTest
    
    * :memo: Add documentation for the GetDaysDiffBetweenDates snippet
    
    * :construction: Add GetMeridiemSuffixOfInteger
    
    * :white_check_mark: Add tests for GetMeridiemSuffixOfInteger
    
    * :memo: Add documentation for GetMeridiemSuffixOfInteger snippet
    
    * :construction: Add IsBusinessDay function
    
    * :white_check_mark: Add tests for IsBusinessDay
    
    * :memo: Add documentation for IsBusinessDay snippet
    
    * :construction: Add Tomorrow function
    
    * :white_check_mark: Add tests for Tomorrow function
    
    * :memo: Add documentation for Tomorrow snippet
    
    * :construction: Add Yesterday method
    
    * :white_check_mark: Add tests for Yesterday method
    
    * :memo: Add documentation for Yesterday snippet
    
    * :construction: Add IsBetweenDates method
    
    * :white_check_mark: Add tests for IsBetweenDates method
    
    * :memo: Add documentation for IsBetweenDates snippet

---
+++ 30 Seconds of CSharp/Date/Yesterday.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_Seconds_of_CSharp.Date
+{
+    public static partial class DateUtils
+    {
+        /// <summary>
+        /// Return a string representation of yesterday's date.
+        /// </summary>
+        /// <returns>The yesterday's date in <c>yyyy-MM-DD</c> format</returns>
+        public static string Yesterday()
+        {
+            return DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd");
+        }
+    }
+}
