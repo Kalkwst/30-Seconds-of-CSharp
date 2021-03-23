41cb9310802bb232e46af5ad1f6e55ce6cf5ad0f
Kalkwst <kalafatiskwstas@gmail.com>
2021-03-23 09:54:49 +0200

    Add Date Features (#2)
    * :construction: Add implementation and tests for FormatDuration function
    
    * :memo: Add FormatDuration snippet documentation
    
    * :memo: Fix formatting of snippet
    
    * :construction: Add IsBeforeDate function
    
    * :white_check_mark: Add tests for IsBeforeDate
    
    * :memo: Add documentation for function
    
    * :memo: Add IsBeforeDate snippet documentation
    
    * :memo: Change formatting of the snippet documentation
    
    * :construction: Add IsAfterDate method
    
    * :white_check_mark: Add tests for IsAfterDate method
    
    * :memo: Add documentation for IsAfterDate snippet
    
    * :construction: Add AddWeekDays method
    
    * :white_check_mark: Add tests for AddWeekDays method
    
    * :memo: Add documentation for AddWeekDays snippet
    
    * :construction: Add DaysAgo method
    
    * :white_check_mark: Add tests for DaysAgo method
    
    * :memo: Add documentation for DaysAgo snippet
    
    * :construction: Add DaysFromNow method
    
    * :white_check_mark: Add tests for DaysFromNow method
    
    * :memo: Add documentation for DaysFromNow snippet

---
+++ 30 Seconds of CSharp/Date/DaysFromNow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _30_Seconds_of_CSharp.Date
+{
+    public static partial class DateUtils
+    {
+        /// <summary>
+        /// Calculates the date of <c>n</c> from the provided day as a string representation.
+        /// </summary>
+        /// <param name="current">The date to start calculating</param>
+        /// <param name="count">The number of days in the future</param>
+        /// <returns>The future date</returns>
+        public static DateTime DaysFromNow(DateTime current, int count)
+        {
+            int days = Math.Abs(count);
+
+            for (int i = 0; i < days; i++)
+            {
+                current = current.AddDays(1);
+            }
+
+            return current;
+        }
+    }
+}
