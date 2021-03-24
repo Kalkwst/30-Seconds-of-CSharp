9fcd72dc62296bd8f14ea30bd537fdd69f01a22f
Kalkwst <kalafatiskwstas@gmail.com>
2021-03-24 14:04:22 +0200

    Merge Math (#5)
    * :white_check_mark: Add tests for ApproximatelyEquals
    
    * :memo: Add documentation for ApproximatelyEqual method

---
+++ 30 Seconds of CSharp Test/Maths/ApproximatelyEqualsTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _30_Seconds_of_CSharp.Maths;
+using NUnit.Framework;
+
+namespace _30_Seconds_of_CSharp_Test.Maths
+{
+    [TestFixture]
+    class MathUtils_ApproximatelyEqualTest
+    {
+        [Test]
+        public void ApproximatelyEqual_PI()
+        {
+            Assert.IsTrue(MathUtils.ApproximatelyEqual(Math.PI / 2.0, 1.5708));
+        }
+
+        [Test]
+        public void ApproximatelyEquals_03()
+        {
+            Assert.IsTrue(MathUtils.ApproximatelyEqual(0.1 + 0.2, 0.3));
+        }
+
+        [Test]
+        public void ApproximatelyEquals_EqualValues()
+        {
+            Assert.IsTrue(MathUtils.ApproximatelyEqual(0.5, 0.5));
+        }
+
+        public void ApproximatelyEquals_CustomEp
+    }
+}
---
+++ 30 Seconds of CSharp/Maths/ApproximatelyEquals.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_Seconds_of_CSharp.Maths
+{
+    public static partial class MathUtils
+    {
+        /// <summary>
+        /// Checks if two numbers are approximately equal to each other.
+        /// </summary>
+        /// <param name="num1">The first number</param>
+        /// <param name="num2">The second number</param>
+        /// <param name="epsilon">The maximum possible difference between the two numbers</param>
+        /// <returns><c>true</c> if the two numbers are appropriately equal</returns>
+        public static bool ApproximatelyEqual(double num1, double num2, double epsilon = 0.001)
+        {
+            return Math.Abs(num1 - num2) < epsilon;
+        }
+    }
+}
