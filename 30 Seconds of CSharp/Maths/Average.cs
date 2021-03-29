00e0f0fa3d918adbc91ac896bae2149db176b784
Kalkwst <kalafatiskwstas@gmail.com>
2021-03-29 11:48:39 +0300

    :sparkles: Add Average nethod (#8)
    * :construction: Add Average method
    
    * :white_check_marks: Add tests for Average
    
    * :memo: Add documentation for Average method

---
+++ 30 Seconds of CSharp/Maths/Average.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_Seconds_of_CSharp.Maths
+{
+    public static partial class MathUtils
+    {
+        /// <summary>
+        /// Calculates the average of two or more numbers.
+        /// </summary>
+        /// <param name="array">The numbers to calculate the average of</param>
+        /// <returns>The average of the provided numbers</returns>
+        public static double Average(params int[] array)
+        {
+            double avg = 0;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                avg += (double)array[i] / array.Length;
+            }
+
+            return avg;
+        }
+
+        /// <summary>
+        /// Calculates the average of two or more numbers.
+        /// </summary>
+        /// <param name="array">The numbers to calculate the average of</param>
+        /// <returns>The average of the provided numbers</returns>
+        public static double Average(params long[] array)
+        {
+            double avg = 0;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                avg += (double)array[i] / array.Length;
+            }
+
+            return avg; 
+        }
+
+        /// <summary>
+        /// Calculates the average of two or more numbers.
+        /// </summary>
+        /// <param name="array">The numbers to calculate the average of</param>
+        /// <returns>The average of the provided numbers</returns>
+        public static decimal Average(params decimal[] array)
+        {
+            decimal avg = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                avg += array[i] / array.Length;
+            }
+
+            return avg;
+        }
+
+        /// <summary>
+        /// Calculates the average of two or more numbers.
+        /// </summary>
+        /// <param name="array">The numbers to calculate the average of</param>
+        /// <returns>The average of the provided numbers</returns>
+        public static double Average(params double[] array)
+        {
+            double avg = 0;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                avg += array[i] / array.Length;
+            }
+
+            return avg;
+        }
+    }
+}
---
+++ docs/Math/snippets/Average.md
@@ -0,0 +1,71 @@
+## Average
+
+Calculates the average of two or more numbers.
+
+- Use a `for` loop to add the result of `array[i]/array.Length` to an `avg` variable.
+- Return the variable
+
+### C#
+
+```csharp
+public static double Average(params int[] array)
+{
+    double avg = 0
+    for(int i = 0; i < array.Length; i++)
+    {
+        avg += (double)array[i] / array.Length;
+    
+    return avg;
+}
+```
+
+```csharp
+public static double Average(params long[] array)
+{
+    double avg = 0;
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        avg += (double)array[i] / array.Length;
+    }
+
+    return avg;
+}
+``` 
+
+```csharp
+public static decimal Average(params decimal[] array)
+{
+    decimal avg = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        avg += array[i] / array.Length;
+    
+    return avg;
+}
+```
+
+```csharp
+public static decimal Average(params decimal[] array)
+{
+    decimal avg = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        avg += array[i] / array.Length;
+    
+    return avg;
+}
+```
+
+### Examples
+
+```csharp
+MathUtils.Average(0);
+//=> 0
+
+MathUtils.Average(1, 2, 3, 4, 5);
+//=> 3.0
+
+MathUtils.Average(-1, -2, -3);
+//=> 2.0
+```
