625dc4e3bf8cd627eedfd6a57dce92f7cc138dda
Kalkwst <kalafatiskwstas@gmail.com>
2021-03-30 10:53:28 +0300

    ✨ Add Binomial Coefficient (#9)
    * :construction: Add BinomialCoefficient method
    
    * :white_check_mark: Add tests for Binomial Coefficient
    
    * :memo: Add documentation for BinomialCoefficient snippet

---
+++ 30 Seconds of CSharp/Maths/BinomialCoefficient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_Seconds_of_CSharp.Maths
+{
+    public static partial class MathUtils
+    {
+        /// <summary>
+        /// Calculates the binomial coefficient between two numbers.
+        /// </summary>
+        /// <param name="n">The first factor</param>
+        /// <param name="k">The second factor</param>
+        /// <returns>The binomial coefficient</returns>
+        public static int BinomialCoefficient(int n, int k)
+        {
+            if (k < 0 || k > n)
+                return 0;
+
+            if (k == 0 || k == n)
+                return 1;
+
+            if (k == 1 || k == n - 1)
+                return n;
+
+            if (n - k < k)
+                k = n - k;
+
+            double res = (double)n;
+
+            for (int i = 2; i <= k; i++)
+                res *= (double)(n - i + 1) / i;
+
+            return (int)Math.Round(res);
+        }
+    }
+}
