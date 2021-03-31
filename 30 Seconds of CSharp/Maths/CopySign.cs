06c9f1593664c980b649903350c0be70eedf80b8
Kalkwst <kalafatiskwstas@gmail.com>
2021-03-31 14:46:37 +0300

    :sparkles: Add CopySign (#12)
    * :construction: Add CopySign nethod
    
    * :white_check_mark: Add tests for CopySign
    
    * :memo: Add documentation for CopySign snippet

---
+++ 30 Seconds of CSharp/Maths/CopySign.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_Seconds_of_CSharp.Maths
+{
+
+    public static partial class MathUtils
+    {
+        /// <summary>
+        /// Returns the absolute value of the first number, but the sign of the second.
+        /// </summary>
+        /// <param name="x">The first number</param>
+        /// <param name="y">The second number</param>
+        /// <returns>the absolute value of the first number, but the sign of the second</returns>
+        public static int CopySign(int x, int y)
+        {
+            return Math.Sign(x) == Math.Sign(y) ? x : -x;
+        }
+
+        /// <summary>
+        /// Returns the absolute value of the first number, but the sign of the second.
+        /// </summary>
+        /// <param name="x">The first number</param>
+        /// <param name="y">The second number</param>
+        /// <returns>the absolute value of the first number, but the sign of the second</returns>
+        public static long CopySign(long x, long y)
+        {
+            return Math.Sign(x) == Math.Sign(y) ? x : -x;
+        }
+
+        /// <summary>
+        /// Returns the absolute value of the first number, but the sign of the second.
+        /// </summary>
+        /// <param name="x">The first number</param>
+        /// <param name="y">The second number</param>
+        /// <returns>the absolute value of the first number, but the sign of the second</returns>
+        public static float CopySign(float x, float y)
+        {
+            return Math.Sign(x) == Math.Sign(y) ? x : -x;
+        }
+
+        /// <summary>
+        /// Returns the absolute value of the first number, but the sign of the second.
+        /// </summary>
+        /// <param name="x">The first number</param>
+        /// <param name="y">The second number</param>
+        /// <returns>the absolute value of the first number, but the sign of the second</returns>
+        public static double CopySign(double x, double y)
+        {
+            return Math.Sign(x) == Math.Sign(y) ? x : -x;
+        }
+
+        /// <summary>
+        /// Returns the absolute value of the first number, but the sign of the second.
+        /// </summary>
+        /// <param name="x">The first number</param>
+        /// <param name="y">The second number</param>
+        /// <returns>the absolute value of the first number, but the sign of the second</returns>
+        public static decimal CopySign(decimal x, decimal y)
+        {
+            return Math.Sign(x) == Math.Sign(y) ? x : -x;
+        }
+    }
+}
